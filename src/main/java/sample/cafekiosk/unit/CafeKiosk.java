b12ff6e92acce9500129b305b767ead4c7f20a1d
nahye <nnhh.shine@gmail.com>
2024-10-15 13:30:39 +0900

    TDD 방식으로 calculateTotalPrice() 구현
    - calculateTotalPrice() 테스트 코드 작성후 기능 구현

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -39,13 +39,13 @@ public class CafeKiosk {
         beverages.clear();
     }
 
+
     public int calculateTotalPrice() {
-        int totalPrice = 0;
-        for(Beverage beverage : beverages){
-            totalPrice += beverage.getPrice();
-        }
-        return totalPrice;
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
     }
+
 
     public Order createOrder(){
         LocalDateTime currentDateTime = LocalDateTime.now();
@@ -65,5 +65,4 @@ public class CafeKiosk {
 
         return new Order(currentDateTime, beverages);
     }
-
 }
