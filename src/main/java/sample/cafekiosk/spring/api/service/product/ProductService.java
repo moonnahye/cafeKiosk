154359cde81a2482adb120ae8c5f8b811b684055
nahye <nnhh.shine@gmail.com>
2024-10-22 15:06:30 +0900

    ProductService에 @Transactional 추가

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
@@ -12,7 +13,15 @@ import sample.cafekiosk.spring.domain.product.ProductType;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
+/**
+ * readOnly = true : 읽기 전용
+ * CRUD 에서 CUD 동작 X / only Read
+ * JPA : CUD 스냅샷 저장, 변경감지 X -> (성능 향상)
+ *
+ * CQRS - Command / Read 를 분리하자
+ */
+
+@Transactional(readOnly = true)@RequiredArgsConstructor
 @Service
 public class ProductService {
 
@@ -20,20 +29,24 @@ public class ProductService {
 
     // 동시성 이슈
     // UUID
+    @Transactional
     public ProductResponse createProduct(ProductCreateRequest request) {
         String nextProductNumber = createNextProductNumber();
 
         Product product = request.toEntity(nextProductNumber);
         Product savedProduct = productRepository.save(product);
 
-        return ProductResponse.builder()
-                .id(savedProduct.getId())
-                .productNumber(nextProductNumber)
-                .type(request.getType())
-                .sellingStatus(request.getSellingStatus())
-                .name(request.getName())
-                .price(request.getPrice())
-                .build();
+        return ProductResponse.of(savedProduct);
+    }
+
+
+    public List<ProductResponse> getSellingProducts(){
+
+        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+
+        return products.stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
     }
 
     private String createNextProductNumber(){
@@ -46,14 +59,5 @@ public class ProductService {
         int nextProductNumberInt = latestProductNumberInt + 1;
 
         return String.format("%03d", nextProductNumberInt);
-    }
-
-    public List<ProductResponse> getSellingProducts(){
-
-        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
-
-        return products.stream()
-                .map(ProductResponse::of)
-                .collect(Collectors.toList());
     }
 }
