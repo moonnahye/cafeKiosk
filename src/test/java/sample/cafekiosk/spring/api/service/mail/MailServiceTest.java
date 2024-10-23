cf7158720059ad0e3335d6b7e6b942f3903e1f53
nahye <nnhh.shine@gmail.com>
2024-10-23 23:52:30 +0900

    MailServiceTest BDDMockito 사용
    - given절 BDDMockito 사용해서 작성

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -3,10 +3,7 @@ package sample.cafekiosk.spring.api.service.mail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
@@ -14,12 +11,13 @@ import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MailServiceTest {
 
-    @Spy
+    @Mock
     private MailSendClient mailSendClient;
 
     @Mock
@@ -34,9 +32,11 @@ class MailServiceTest {
         // given
 //        when(mailSendClient.sendEmail(anyString(),anyString(),anyString(),anyString()))
 //                .thenReturn(true);
-        doReturn(true)
-                .when(mailSendClient)
-                .sendEmail(anyString(),anyString(),anyString(),anyString());
+        given(mailSendClient.sendEmail(anyString(),anyString(),anyString(),anyString()))
+                .willReturn(true);
+//        doReturn(true)
+//                .when(mailSendClient)
+//                .sendEmail(anyString(),anyString(),anyString(),anyString());
 
         // when
         boolean result = mailService.sendMail("", "", "", "");
